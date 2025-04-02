6d220b470cbd82651d65f866efa83145c5911cfb
Nadhir Bakare <bakarenadhir2005@gmail.com>
2025-04-02 19:48:23 -0400

    I added string replacements to substitute '94 fahrenheit' with the temperature variable and '300 pounds' with the weight variable

--- Assignment4-part1/main.js
+++ Assignment4-part1/main.js
@@ -34,7 +34,7 @@ newStory = newStory.replace("Bob", name);
   if(document.getElementById("uk").checked) {
     const weight = `${Math.round(300/14)}stone`;
     const temperature = `${Math.round((94-32) * (5/9))}centigrade`;
-
+    newStory = newStory.replace("300 pounds", weight).replace("94 fahrenheit", temperature);
   }
 
   story.textContent = newStory;
