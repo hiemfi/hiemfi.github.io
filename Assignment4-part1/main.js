43b77dca37ff438fe3eb4343830a02670d3f3328
Nadhir Bakare <bakarenadhir2005@gmail.com>
2025-04-02 19:17:25 -0400

    I added a brief description to the html file

--- Assignment4-part1/main.js
+++ Assignment4-part1/main.js
@@ -10,7 +10,6 @@ function randomValueFromArray(array){
 
 const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
-
 const insertY = ["the soup kitchen", "Disneyland", "the white house"];
 const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
@@ -24,9 +23,12 @@ let newStory = storyText;
 const xItem = randomValueFromArray(insertX);
 const yItem = randomValueFromArray(insertY);
 const zItem = randomValueFromArray(insertZ);
+
+
+newStory = newStory.replaceAll(":insertx:", xItem).replace(":inserty:", yItem).replace(":insertz:", zItem);
   if(customName.value !== '') {
     const name = customName.value;
-
+newStory = newStory.replace("Bob", name);
   }
 
   if(document.getElementById("uk").checked) {
@@ -35,6 +37,7 @@ const zItem = randomValueFromArray(insertZ);
 
   }
 
-  story.textContent = ;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }  
+
