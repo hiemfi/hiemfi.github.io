63d21ab1e048697be08e6ad3cf0e8401a2172143
Nadhir Bakare <bakarenadhir2005@gmail.com>
2025-04-04 09:46:33 -0400

    these lines of code loop through each image

--- assignment4-part2/main.js
+++ assignment4-part2/main.js
@@ -11,6 +11,12 @@ const imageFilenames = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg', 'pic5.jpg']
 const altTexts = {'pics1.jpg': 'Closeup of a blue human eye', 'pic2.jpg': 'A beach at sunset','pic3.jpg':'A beautiful mountain landscape','pic4.jpg':'A city skyline at night', 'pic5.jpg':'A forest with morning fog'}
 
 /* Looping through images */
+imageFilenames.forEach(function(filename)){
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src',`images/${filename}`)
+    newImage.setAttribute('alt', altTexts[filename]);
+}
+
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
