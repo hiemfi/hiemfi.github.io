9446550abc2e2cbcb57a68caa37a360dcfc60efe
Nadhir Bakare <bakarenadhir2005@gmail.com>
2025-04-04 09:41:40 -0400

    Addinf alternative text for each image file for accessibility purposes

--- assignment4-part2/main.js
+++ assignment4-part2/main.js
@@ -5,11 +5,10 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const imageFilenames = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg', 'pic5.jpg',];
-
-
+const imageFilenames = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const altTexts = {'pics1.jpg': 'Closeup of a blue human eye', 'pic2.jpg': 'A beach at sunset','pic3.jpg':'A beautiful mountain landscape','pic4.jpg':'A city skyline at night', 'pic5.jpg':'A forest with morning fog'}
 
 /* Looping through images */
 
